c9d3f3eb1ae28a1d873fd9bb8c834aeb533397e7
brent-yoast <brent@yoast.com>
2022-06-10 11:49:28 +0200

    protobuf restriction again

--- setup.py
+++ setup.py
@@ -16,7 +16,8 @@ setup(
         'singer-python~=5.10.0',
         'google-cloud-bigquery~=3.0.0',
         'pandas~=1.3.5',
-        'db-dtypes~=1.0.0'
+        'db-dtypes~=1.0.0',
+        'protobuf==3.19.0'
     ],
     entry_points="""
         [console_scripts]
